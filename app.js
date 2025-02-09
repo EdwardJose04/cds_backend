76325ccda81363f8abfbc7a1b39e495c1fd32f7f
Edward Rangel <edwardjoserangel@gmail.com>
2025-02-08 22:11:17 -0500

    Controller y routes para productos añadido

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(express.json());
 // Rutas Base
 app.use('/api/usuarios', require('./routes/usuarios.routes'));
 app.use('/api/herramientas', require('./routes/herramientas.routes'));
-/* app.use('/api/productos', require('./routes/productos.routes'));
-app.use('/api/prestamos', require('./routes/prestamos.routes'));
+ app.use('/api/productos', require('./routes/productos.routes'));
+/* app.use('/api/prestamos', require('./routes/prestamos.routes'));
 app.use('/api/reportes', require('./routes/reportes.routes')); */
 
 // Manejo de errores global
